8e788aa3b1ca7a2e31674a60ec70032b505caf8f
Esmaeil <smaelll7554@gmail.com>
2023-04-25 12:23:35 +0330

    only video stream encoded others stream demuxed without change

--- compress.cpp
+++ compress.cpp
@@ -16,48 +16,53 @@ struct codec_information
     AVCodecContext *video_codec_context;
     int video_stream_index;
     int audio_stream_index;
+    AVPacket *packet;
+    AVFrame *frame;
 } ;
-void debug(std::string message,int result);
+void debug(std::string message,int ret,bool doExit = true);
 void set_video_encoder_context(AVCodecContext *decoder,AVCodecContext *encoder);
 void set_audio_encoder_context(AVCodecContext *decoder,AVCodecContext *encoder);
 AVFormatContext * openMedia(char *inputFilePath);
 void set_decoder(codec_information &decoder, AVFormatContext *inputFormatContext);
-void set_encoder(codec_information decoder,codec_information &encoder);
+void set_encoder(codec_information decoder,codec_information &encoder, AVFormatContext *ofmt_ctx);
+void write_encoded_frame(codec_information &encoder, AVFormatContext *ofmt_ctx,AVFormatContext *ifmt_ctx, AVCodecContext *target);
 int main()
 {
-    std::string inputFilePath = "sample.mkv";
-    std::string outputFilePath = "output1.mp4";
+    std::string inputFilePath = "test.mp4";
+    std::string outputFilePath = "output.mp4";
     AVFormatContext* inputFormatContext = nullptr;
-    int result,result_ = -1;
-
+    int ret;
     inputFormatContext = openMedia((char *)inputFilePath.c_str());
+
     codec_information decoder;
-    codec_information encoder;
     set_decoder(decoder,inputFormatContext);
     decoder.video_codec_context->framerate = av_guess_frame_rate(inputFormatContext, inputFormatContext->streams[decoder.video_stream_index], NULL);
+
     AVFormatContext* outputFormatContext = nullptr;
-    result = avformat_alloc_output_context2(&outputFormatContext, nullptr, nullptr, outputFilePath.c_str());
-    if ( result< 0)
+    ret = avformat_alloc_output_context2(&outputFormatContext, nullptr, nullptr, outputFilePath.c_str());
+    if ( ret< 0)
     {
         // error handling
-        debug("avformat_alloc_output_context2",result);
+        debug("avformat_alloc_output_context2",ret);
     }
+    codec_information encoder;
+    set_encoder(decoder,encoder,outputFormatContext);
 
-    set_encoder(decoder,encoder);
-
-    AVStream* stream = avformat_new_stream(outputFormatContext, encoder.video_codec);
-    if (stream == nullptr)
+    AVStream* video_stream = avformat_new_stream(outputFormatContext, encoder.video_codec);
+    AVStream* audio_stream = avformat_new_stream(outputFormatContext, decoder.audio_codec);
+    ret = avcodec_parameters_from_context(video_stream->codecpar, encoder.video_codec_context);
+    if(ret < 0)
     {
-        // error handling
-        debug("avformat_new_stream",-1);
+        debug("video_stream avcodec_parameters_from_context",ret);
     }
-
-    result = avcodec_parameters_from_context(stream->codecpar, encoder.video_codec_context);
-    if(result < 0)
+    ret = avcodec_parameters_from_context(audio_stream->codecpar, decoder.audio_codec_context);
+    if(ret < 0)
     {
-        debug("avcodec_parameters_from_context",result);
+        debug("audio_stream avcodec_parameters_from_context",ret);
     }
-    stream->time_base = encoder.video_codec_context->time_base;
+    video_stream->time_base.num = 128000;
+    video_stream->time_base.den = 1;
+    audio_stream->time_base = decoder.audio_codec_context->time_base;
     std::cout<<"\n";
     av_dump_format(outputFormatContext, 0, outputFilePath.c_str(), 1);
 
@@ -71,111 +76,99 @@ int main()
         // error handling
         debug("avformat_write_header",-1);
     }
-
-    AVPacket packet;
-    av_init_packet(&packet);
-    packet.data = nullptr;
-    packet.size = 0;
-
-    AVFrame* frame = av_frame_alloc();
-    if (frame == nullptr)
-    {
-        // error handling
-        std::cout<<"av_frame_alloc";
-        exit(23);
-    }
-    AVFrame *input_frame = av_frame_alloc();
-    AVPacket *input_packet = av_packet_alloc();
-    // used function
-
+    decoder.packet = av_packet_alloc();
+    decoder.packet->data = nullptr;
+    decoder.packet->size = 0;
+    decoder.frame = av_frame_alloc();
     std::cout<<"\n\t\t\t~~~~~~~~~~~~~~~~~~~Transcodeing~~~~~~~~~~~~~~~~~~~";
     std::cout<<"\nFrame"<<"\tPTS"<<"\tDTS"<<"\tKey_Frame"<<"\tCoded_Picture_Number"<<"\tDisplay_Picture_Number"<<"\tWidth"<<"\tHeight"<<"\tFormat";
-    while (av_read_frame(inputFormatContext, &packet) >= 0)
+    while(true)
     {
-        if (packet.stream_index == decoder.video_stream_index)
+        ret = av_read_frame(inputFormatContext,decoder.packet);
+        if(ret < 0)
         {
-            AVFrame* decodedFrame = av_frame_alloc();
-            if (decodedFrame == nullptr)
+            debug("av_read_frame",ret,false);
+            break;
+        }
+        if (decoder.packet->stream_index == decoder.video_stream_index)
+        {
+            ret = avcodec_send_packet(decoder.video_codec_context, decoder.packet);
+            if(ret >= 0)
             {
-                // error handling
-                debug("decodedFrame == nullpt",-1);
-            }
-
-            int frameFinished = 0;
-            result = avcodec_send_packet(decoder.video_codec_context, &packet);
-            result_ = avcodec_receive_frame(decoder.video_codec_context, decodedFrame);
-            if ( result== 0 &&  result_== 0)
-            {
-                std::cout<<"\n  "<<av_get_picture_type_char(decodedFrame->pict_type);
-                std::cout<<"\t "<<decodedFrame->pts;
-                std::cout<<"\t "<<decodedFrame->pkt_dts<<"\t    "<<decodedFrame->key_frame<<"\t\t\t ";
-                std::cout<<decodedFrame->coded_picture_number<<"\t\t\t   "<<decodedFrame->display_picture_number<<"\t\t "<<decodedFrame->width<<"\t "<<decodedFrame->height<<"\t  "<<decodedFrame->format;
-                //save_gray_frame(decodedFrame->data[0], decodedFrame->linesize[0], decodedFrame->width, decodedFrame->height, (char *)frame_filename.c_str());
-                //av_frame_unref(frame);
-                //av_frame_move_ref(frame, decodedFrame);
-                //av_packet_unref(&packet);
-                //frame.
-                /*
-                std::cout<<"\nencoder frame:";
-                std::cout<<"\nFrame "<<av_get_picture_type_char(frame->pict_type);
-                std::cout<<" "<<decoder.video_codec_context->frame_number<<" pts "<<frame->pts;
-                std::cout<<" dts "<<frame->pkt_dts<<" key_frame "<<frame->key_frame<<" [coded_picture_number ";
-                std::cout<<frame->coded_picture_number<<" , display_picture_number "<<frame->display_picture_number<<"]"<<frame->width<<"x"<<frame->height<<" "<<frame->format;
-*/
-                AVPacket encodedPacket;
-                av_init_packet(&encodedPacket);
-                encodedPacket.data = nullptr;
-                encodedPacket.size = 0;
-
-                result = avcodec_send_frame(encoder.video_codec_context, decodedFrame);
-                result_ = avcodec_receive_packet(encoder.video_codec_context, &encodedPacket);
-                if ( result== 0 &&  result_== 0)
+                while(true)
                 {
-                    encodedPacket.stream_index = stream->index;
-                    /*av_packet_rescale_ts(&packet,
-                                 inputFormatContext->streams[stream->index]->time_base,
-                                 outputFormatContext->streams[stream->index]->time_base);*/
-
-                    result = av_interleaved_write_frame(outputFormatContext, &encodedPacket);
-
-                    if ( result< 0)
+                    ret = avcodec_receive_frame(decoder.video_codec_context, decoder.frame);
+                    if(ret < 0)
                     {
-                        // error handling
-                        debug("decodedFrame == nullpt",result);
+                        //debug("avcodec_receive_frame",ret,false);
+                        break;
                     }
+                    encoder.frame = decoder.frame;
+                    encoder.packet = av_packet_alloc();
+                    encoder.packet->data = nullptr;
+                    encoder.packet->size = 0;
+                    encoder.packet->stream_index = decoder.packet->stream_index;
 
-                    av_packet_unref(&encodedPacket);
-                }
-                else
-                {
-
-                    std::cout<<"\nSEND_FRAME_ERROR: ";
-                    debug("avcodec_send_frame",result);
+                    std::cout<<"\n  "<<av_get_picture_type_char(encoder.frame->pict_type);
+                    std::cout<<"\t "<<encoder.frame->pts;
+                    std::cout<<"\t "<<encoder.frame->pkt_dts<<"\t    "<<encoder.frame->key_frame<<"\t\t\t ";
+                    std::cout<<encoder.frame->coded_picture_number<<"\t\t\t   "<<encoder.frame->display_picture_number<<"\t\t "<<encoder.frame->width<<"\t "<<encoder.frame->height<<"\t  "<<encoder.frame->format<<"\t"<<encoder.frame->best_effort_timestamp;
+                    write_encoded_frame(encoder, outputFormatContext,inputFormatContext,encoder.video_codec_context);
                 }
             }
             else
             {
-                std::cout<<"\nSEND_PACKET_ERROR: ";
-                debug("avcodec_send_packet",result);
+                debug("avcodec_send_packet",ret);
             }
-
-            av_frame_free(&decodedFrame);
-        }
+        }/*else if(decoder.packet->stream_index == decoder.audio_stream_index){
+            ret = av_interleaved_write_frame(outputFormatContext, decoder.packet);
+            if(ret < 0){
+                debug("av_interleaved_write_frame",ret,true);
+            }
+        }*/
     }
     av_write_trailer(outputFormatContext);
 }
+void write_encoded_frame(codec_information &encoder, AVFormatContext *ofmt_ctx,AVFormatContext *ifmt_ctx,AVCodecContext *target)
+{
+    int ret;
+    av_packet_unref(encoder.packet);
+    encoder.frame->pts = encoder.frame->best_effort_timestamp;
+    ret = avcodec_send_frame(target,encoder.frame);
+    if(ret >= 0)
+    {
+        while(true)
+        {
+            ret = avcodec_receive_packet(target,encoder.packet);
+            if (ret == AVERROR_EOF || ret == AVERROR(EAGAIN))
+                break;
+            else if (ret < 0)
+            {
+                debug("avcodec_receive_packet",ret,true);
+            }
+            //encoder.packet.pts
+            ret = av_interleaved_write_frame(ofmt_ctx, encoder.packet);
+            if(ret < 0)
+            {
+                debug("av_interleaved_write_frame",ret,true);
+            }
+        }
+    }
+}
 void set_video_encoder_context(AVCodecContext *decoder,AVCodecContext *encoder)
 {
-    std::cout<<"\nVideo Context: \n"<<"\theight"<<"\twidth"<<"\tpix_fmt"<<"\tbit_rate"<<"\trc_buffer_size"<<"\trc_max_rate"<<"\trc_min_rate"<<"\tflags"<<"\n";
-    std::cout<<"\t"<< decoder->height<<"\t"<<decoder->width <<"\t   "<< decoder->pix_fmt<<"\t "<<decoder->bit_rate;
-    std::cout<<"\t\t\t"<< decoder->rc_buffer_size<<"\t  "<<decoder->rc_max_rate <<"\t   "<<decoder->rc_min_rate<<"\t\t  "<< decoder->flags;
     encoder->height = decoder->height;
     encoder->width = decoder->width;
     encoder->sample_aspect_ratio = decoder->sample_aspect_ratio;
-    encoder->bit_rate = 400000/*decoder->bit_rate*/;
-    encoder->rc_min_rate = decoder->rc_min_rate;
-    encoder->flags = decoder->flags;
+    //encoder->flags = decoder->flags;
     encoder->time_base = av_inv_q(decoder->framerate);
+    //int m_bitRate = decoder->bit_rate;
+    //encoder->bit_rate = m_bitRate;
+    //int delta = 0;
+    //encoder->rc_min_rate = m_bitRate;
+    //encoder->rc_max_rate = m_bitRate + delta;
+    //encoder->rc_buffer_size = 2000000;
+    //encoder->bit_rate_tolerance = delta;
 }
 void set_audio_encoder_context(AVCodecContext *decoder,AVCodecContext *encoder)
 {
@@ -189,12 +182,12 @@ void set_audio_encoder_context(AVCodecContext *decoder,AVCodecContext *encoder)
     encoder->channel_layout = decoder->channel_layout;
     encoder->time_base = (AVRational)(decoder->time_base);
 }
-void set_encoder(codec_information decoder,codec_information &encoder)
+void set_encoder(codec_information decoder,codec_information &encoder, AVFormatContext *ofmt_ctx)
 {
-    std::cout<<"\n\nSetting encoders ...";
-    int result;
+    std::cout<<"\n\nSetting encoders ..."<<decoder.video_codec_context->b_frame_strategy;
+    int ret;
     //video encoder
-    encoder.video_codec = avcodec_find_encoder_by_name(decoder.video_codec->name);
+    encoder.video_codec = avcodec_find_encoder(decoder.video_codec->id);
     encoder.video_codec_context = avcodec_alloc_context3(encoder.video_codec );
     if(encoder.video_codec == nullptr)
     {
@@ -202,19 +195,25 @@ void set_encoder(codec_information decoder,codec_information &encoder)
         exit(200);
     }
     std::cout<<"\nVideo Encoder: "<<encoder.video_codec->name;
-    set_video_encoder_context(decoder.video_codec_context,encoder.video_codec_context);
+    /*set_video_encoder_context(decoder.video_codec_context,encoder.video_codec_context);
     if (encoder.video_codec->pix_fmts)
         encoder.video_codec_context->pix_fmt = encoder.video_codec->pix_fmts[0];
     else
-        encoder.video_codec_context->pix_fmt = decoder.video_codec_context->pix_fmt;
-    std::cout<<"\nTimeBase: "<<encoder.video_codec_context->time_base.den<<"(FPS)"<< "\t"<<encoder.video_codec_context->time_base.num;
-    result = avcodec_open2(encoder.video_codec_context, encoder.video_codec, NULL);
-    if(result < 0)
+        encoder.video_codec_context->pix_fmt = decoder.video_codec_context->pix_fmt;*/
+    avcodec_copy_context(encoder.video_codec_context,decoder.video_codec_context);
+    if (ofmt_ctx->oformat->flags & AVFMT_GLOBALHEADER)
+        encoder.video_codec_context->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+    ret = avcodec_open2(encoder.video_codec_context, encoder.video_codec, NULL);
+    if(ret < 0)
     {
-        debug("video encoder",result);
+        debug("video encoder",ret);
     }
+    std::cout<<"\nVideo Context: \n"<<"\theight"<<"\twidth"<<"\tpix_fmt"<<"\tbit_rate"<<"\trc_buffer_size"<<"\trc_max_rate"<<"\trc_min_rate"<<"\tflags"<<"\n";
+    std::cout<<"\t"<< encoder.video_codec_context->height<<"\t"<<encoder.video_codec_context->width <<"\t   "<< encoder.video_codec_context->pix_fmt<<"\t "<<encoder.video_codec_context->bit_rate;
+    std::cout<<"\t\t\t"<< encoder.video_codec_context->rc_buffer_size<<"\t  "<<encoder.video_codec_context->rc_max_rate <<"\t   "<<encoder.video_codec_context->rc_min_rate<<"\t\t  "<< encoder.video_codec_context->flags;
+    std::cout<<"\nTimeBase: "<<encoder.video_codec_context->time_base.den<<"(FPS)"<< "\t"<<encoder.video_codec_context->time_base.num;
     //audio encoder
-    encoder.audio_codec = avcodec_find_encoder_by_name(decoder.audio_codec->name);
+    encoder.audio_codec = avcodec_find_encoder(decoder.audio_codec->id);
     encoder.audio_codec_context = avcodec_alloc_context3(encoder.audio_codec);
     if(encoder.audio_codec == nullptr)
     {
@@ -224,17 +223,17 @@ void set_encoder(codec_information decoder,codec_information &encoder)
     std::cout<<"\nAudio Encoder: "<<encoder.audio_codec->name;
     set_audio_encoder_context(decoder.audio_codec_context,encoder.audio_codec_context);
     std::cout<<"\nTimeBase: "<<encoder.audio_codec_context->time_base.den<<"(FPS)"<< "\t"<<encoder.audio_codec_context->time_base.num;
-    result = avcodec_open2(encoder.audio_codec_context, encoder.audio_codec, NULL);
-    if(result < 0)
+    ret = avcodec_open2(encoder.audio_codec_context, encoder.audio_codec, NULL);
+    if(ret < 0)
     {
-        debug("Audio Encoder",result);
+        debug("Audio Encoder",ret);
     }
 
 
 }
 void set_decoder(codec_information &decoder, AVFormatContext *inputFormatContext)
 {
-    int result;
+    int ret;
     std::cout<<"\n--------------------------DECODERS--------------------------";
     std::cout<<"\nTYPE "<<"\tNAME "<<"\t\tID"<<"\twidth"<<"\theight"<<"\tbit_rate"<<"\tframe_rate(fps,...)";
     AVCodecParameters *codecPar = nullptr;
@@ -245,14 +244,16 @@ void set_decoder(codec_information &decoder, AVFormatContext *inputFormatContext
         {
             decoder.video_codec = avcodec_find_decoder(codecPar->codec_id);
             decoder.video_codec_context = avcodec_alloc_context3(decoder.video_codec);
-            result = avcodec_parameters_to_context(decoder.video_codec_context ,inputFormatContext->streams[i]->codecpar);
-            if(result < 0)
-                debug("avcodec_parameters_to_context",result);
+            ret = avcodec_parameters_to_context(decoder.video_codec_context,inputFormatContext->streams[i]->codecpar);
+            if(ret < 0)
+                debug("avcodec_parameters_to_context",ret);
             decoder.video_codec_parameters = codecPar;
             decoder.video_stream_index = i;
+            decoder.video_codec_context->framerate = av_guess_frame_rate(inputFormatContext, inputFormatContext->streams[i], NULL);
             std::cout<<"\nVIDEO"<<"\t"<<decoder.video_codec->long_name<<"\t"<<decoder.video_codec->id;
             std::cout<<"\t "<<decoder.video_codec_parameters->width <<"\t "<< decoder.video_codec_parameters->height;
             std::cout<<"\t "<<decoder.video_codec_parameters->bit_rate<<"\t\t   "<<decoder.video_codec_context->framerate.num<<","<<decoder.video_codec_context->framerate.den;
+            std::cout<<"\n"<<decoder.video_codec_context->bits_per_coded_sample<< " , "<<decoder.video_codec_context->bits_per_raw_sample;
         }
         else
         {
@@ -265,16 +266,18 @@ void set_decoder(codec_information &decoder, AVFormatContext *inputFormatContext
             std::cout<<"\t "<<decoder.audio_codec_parameters->bit_rate<<"\t\t   "<<decoder.audio_codec_context->framerate.num<<","<<decoder.audio_codec_context->framerate.den;
         }
     }
-    result = avcodec_open2(decoder.video_codec_context, decoder.video_codec, NULL);
-    if(result < 0)
+    ret = avcodec_open2(decoder.video_codec_context, decoder.video_codec, NULL);
+    if(ret < 0)
     {
-        debug("video codec",result);
+        debug("video codec",ret);
     }
-    result = avcodec_open2(decoder.audio_codec_context, decoder.audio_codec, NULL);
-    if(result < 0)
+    ret = avcodec_open2(decoder.audio_codec_context, decoder.audio_codec, NULL);
+    if(ret < 0)
     {
-        debug("audio codec",result);
+        debug("audio codec",ret);
     }
+    std::cout<<"\n";
+    av_dump_format(inputFormatContext, 0, "test.mp4", 0);
     std::cout<<"\nDecoders are Set";
 
 }
@@ -282,27 +285,28 @@ AVFormatContext * openMedia(char *inputFilePath)
 {
     AVFormatContext *inputFormatContext = nullptr;
     std::cout<<"OPENNING MEDIA FILE: "<<inputFilePath<<"\n";
-    int result;
-    result = avformat_open_input(&inputFormatContext, inputFilePath, nullptr, nullptr);
-    if ( result != 0)
+    int ret;
+    ret = avformat_open_input(&inputFormatContext, inputFilePath, nullptr, nullptr);
+    if ( ret != 0)
     {
-        debug("avformat_open_input",result);
+        debug("avformat_open_input",ret);
     }
     std::cout<<"Format "<<inputFormatContext->iformat->long_name<<"\nDuration: "<<inputFormatContext->duration;
-    result = avformat_find_stream_info(inputFormatContext, nullptr);
-    if (result < 0)
+    ret = avformat_find_stream_info(inputFormatContext, nullptr);
+    if (ret < 0)
     {
-        debug("avformat_find_stream_info",result);
+        debug("avformat_find_stream_info",ret);
     }
     std::cout<<"\nBitRate: "<<inputFormatContext->bit_rate;
     std::cout<<"\nMEDIA OPEN FINISHED";
     return inputFormatContext;
 }
-void debug(std::string message, int result)
+void debug(std::string message, int ret,bool doExit)
 {
     char error[500];
-    av_strerror(result, error, 500);
+    av_strerror(ret, error, 500);
     std::cout<<"\nhas a " + message + " error: "+error;
-    exit(50);
+    if(doExit)
+        exit(50);
 }
 
